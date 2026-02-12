7744467195f868a1e20df6502c2308021f60f4dd
ProfCastello <gustavo.castello@sp.senai.br>
2026-02-12 13:53:23 -0300

    feat: remover estilos e componentes não utilizados, adicionar componente Perfil

---
+++ 03-devlinks/src/components/Perfil/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Perfil = () => {
+  return (
+    <div className="perfil">
+      <img src="https://placehold.co/200x200" alt="" />
+      <p>@Seu Nome</p>
+    </div>
+  );
+};
+
+export default Perfil;

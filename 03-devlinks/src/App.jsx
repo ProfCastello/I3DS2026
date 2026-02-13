22fd114e6ed607d442d36d2a0df605ccd907803a
ProfCastello <gustavo.castello@sp.senai.br>
2026-02-13 08:30:53 -0300

    feat: atualizar estrutura do projeto com novos componentes e estilos

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
+import Link from "./components/Link/Link";
 import Perfil from "./components/Perfil/Perfil";
+import Rodape from "./components/Rodape/Rodape";
+import SocialLink from "./components/SocialLink/SocialLink";
 
 function App() {
   return (
     <div id="App">
-      <Perfil />
+      <Perfil fotoPerfil={"https://placehold.co/100"}>Gustavo Castello</Perfil>
 
-      
       <div className="switch">botão switch</div>
-      <div className="links"></div>
-      <div className="socialLinks"></div>
-      <div className="rodape"></div>
+
+      <ul>
+        <Link url={""}>Inscreva-se</Link>
+        <Link url={""}>Minha playlist</Link>
+        <Link url={""}>Me pague um café!</Link>
+        <Link url={""}>Conheça o Curso DEV</Link>
+      </ul>
+
+      <div className="socialLinks">
+        <SocialLink url={"https://github.com"} icon={"logo-github"} />
+        <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
+        <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
+        <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"} />
+      </div>
+      <Rodape>ProfCastello</Rodape>
     </div>
   );
 }
---
+++ 03-devlinks/src/components/Rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Rodape = ({ children }) => {
+  return (
+    <footer>
+      <p>
+        Feito com 🫰🏻por <a href="https://github.com">{children}</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;

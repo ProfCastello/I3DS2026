ed25980b399a2abec988056c7e723b11df3add62
ProfCastello <gustavo.castello@sp.senai.br>
2026-02-19 17:04:57 -0300

    feat: implementar busca de filmes com integração à API OMDB e adicionar componente MovieCard

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,21 +1,49 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
+
 import Rodape from "./components/Rodape/Rodape";
+import MovieCard from "./components/MovieCard/MovieCard";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json;
+
+    //Alimentando a variavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
     <div id="App">
       <img
-        id="logo"
+        id="Logo"
         src={logo}
         alt="Logotipo do serviço de streaming Devflix, com letras vermelhas e fundo preto, promovendo conteúdo de séries, filmes e entretenimento online."
       />
 
       <div className="search">
-        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <input type="text" placeholder="Pesquise por filmes" />
         <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+      <div className="container">
+        {movies.map((movie, index) => (
+          <MovieCard key={index} {...movie} />
+        ))}
       </div>
 
       <Rodape link={"https://github.com/ProfCastello"}>ProfCastello</Rodape>

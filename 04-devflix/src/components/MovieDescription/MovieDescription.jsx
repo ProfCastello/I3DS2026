0e098a22b67ca95bf1638948e94f7c9a36c0264a
ProfCastello <gustavo.castello@sp.senai.br>
2026-02-20 16:46:16 -0300

    feat: adicionar apiUrl ao componente MovieCard e implementar busca de descrição do filme

--- 04-devflix/src/components/MovieDescription/MovieDescription.jsx
+++ 04-devflix/src/components/MovieDescription/MovieDescription.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import styles from "./MovieDescription.module.css";
 
 const MovieDescription = (props) => {
+  const [movieDesc, setMovieDesc] = useState([]);
+
+  useEffect(() => {
+    fetch(`${props.apiUrl}&i=${props.movieID}`)
+      .then((response) => response.json())
+      .then((data) => setMovieDesc(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className={styles.modalBackdrop} onClick={props.click}>
-      <div className={styles.movieModal} onClick={() => ""}>
+      <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
         <div className={styles.movieInfo}>
-            Funcionou
+          <img src={movieDesc.Poster} alt="" />
         </div>
       </div>
     </div>

0597cecc8c0944d380ad94f3f4cff2d85c3e93ff
ProfCastello <gustavo.castello@docente.senai.br>
2026-04-23 13:28:43 -0300

    feat: implement Join component with username input and socket connection

--- 06-devChat/client/src/components/Join/Join.jsx
+++ 06-devChat/client/src/components/Join/Join.jsx
@@ -1,9 +1,33 @@
-import style from "./Join.module.css"
+import { useRef } from "react";
+import style from "./Join.module.css";
+
+import { Input, Button } from "@mui/material";
+import io from "socket.io-client";
 
 const Join = () => {
-  return (
-    <div>Join</div>
-  )
-}
+  const usernameRef = useRef();
 
-export default Join
+  const handleSubmit = async () => {
+    const username = usernameRef.current.value;
+    if (!username.trim()) return;
+
+    const socket = io.connect("http://localhost:3001");
+    socket.emit("set_username", username);
+  };
+
+  return (
+    <div>
+      <h2>Bem-vindo ao devChat</h2>
+      <Input inputRef={usernameRef} placeholder="Nome de usuário" />
+      <Button
+        sx={{ mt: 2, mb: 2 }}
+        variant="contained"
+        onClick={() => handleSubmit()}
+      >
+        Entrar
+      </Button>
+    </div>
+  );
+};
+
+export default Join;
